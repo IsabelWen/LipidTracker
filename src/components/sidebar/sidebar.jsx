8d4e4e97e89f309da2f6780ec14958d0bfd82584
Isy <isy1997@freenet.de>
2024-02-29 19:33:35 +0100

    Add more styling to sidebar

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -3,12 +3,12 @@ import "./sidebar.scss"
 import { Link } from "react-router-dom";
 
 // Import Icons
-// import DashboardIcon from "@mui/icons-material/Dashboard";
-// import StickyNote2Outlined from "@mui/icons-material/StickyNote2Outlined"
-// import BiotechOutlinedIcon from '@mui/icons-material/BiotechOutlined';
-// import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
-// import ExitToAppIcon from "@mui/icons-material/ExitToApp";
-// import QuizOutlinedIcon from '@mui/icons-material/QuizOutlined';
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import StickyNote2Outlined from "@mui/icons-material/StickyNote2Outlined"
+import BiotechOutlinedIcon from '@mui/icons-material/BiotechOutlined';
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import QuizOutlinedIcon from '@mui/icons-material/QuizOutlined';
 
 // Main
 const Sidebar = () => {
@@ -25,42 +25,42 @@ const Sidebar = () => {
                     {/* Dashboard */}
                     <Link to="/" style={{ textDecoration: "none" }}>
                         <li>
-                            {/*<DashboardIcon className="icon" />*/}
+                            <DashboardIcon className="icon" />
                             <span>Dashboard</span>
                         </li>
                     </Link>
                     {/* Test Results */}
                     <Link to="/results" style={{ textDecoration: "none" }}>
                         <li>
-                            {/*<BiotechOutlinedIcon className="icon" />*/}
+                            <BiotechOutlinedIcon className="icon" />
                             <span>Test Results</span>
                         </li>
                     </Link>
                     {/* Notes */}
                     <Link to="/notes" style={{ textDecoration: "none" }}>
                         <li>
-                            {/*<StickyNote2Outlined className="icon" />*/}
+                            <StickyNote2Outlined className="icon" />
                             <span>Notes</span>
                         </li>
                     </Link>
                     {/* FAQ */}
                     <Link to="/faq" style={{ textDecoration: "none" }}>
                         <li>
-                            {/*<QuizOutlinedIcon className="icon" />*/}
+                            <QuizOutlinedIcon className="icon" />
                             <span>FAQ</span>
                         </li>
                     </Link>
                     {/* Settings */}
                     <Link to="/settings" style={{ textDecoration: "none" }}>
                         <li>
-                            {/*<SettingsApplicationsIcon className="icon" />*/}
+                            <SettingsApplicationsIcon className="icon" />
                             <span>Settings</span>
                         </li>
                     </Link>
                     {/* Logout */}
                     <Link to="/" style={{ textDecoration: "none" }}>
                         <li>
-                            {/*<ExitToAppIcon className="icon" />*/}
+                            <ExitToAppIcon className="icon" />
                             <span>Logout</span>
                         </li>
                     </Link>
