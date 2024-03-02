1ebd1fb080a2fc1f3d06bf4c6dec93abbadaf05a
Isy <isy1997@freenet.de>
2024-03-02 17:20:14 +0100

    Create formSource.js

---
+++ src/formSource.js
@@ -0,0 +1,38 @@
+export const resultsInputs = [
+    {
+      id: "date",
+      date: "Date",
+      type: "date",
+      placeholder: "Test Result Date",
+    },
+    {
+      id: "cholesterol",
+      label: "Cholesterol",
+      type: "number",
+      placeholder: "Cholesterol",
+    },
+    {
+      id: "hdl",
+      label: "HDL-Cholesterol",
+      type: "text",
+      placeholder: "HDL-Cholesterol",
+    },
+    {
+      id: "ldl",
+      label: "LDL-Cholesterol",
+      type: "number",
+      placeholder: "LDL-Cholesterol",
+    },
+    {
+      id: "triglycerides",
+      label: "Triglycerides",
+      type: "number",
+      placeholder: "Triglycerides"
+    },
+    {
+      id: "note",
+      label: "Note",
+      type: "text",
+      placeholder: "Type in additional information, such as changes in medication or dietery."
+    },
+  ];
