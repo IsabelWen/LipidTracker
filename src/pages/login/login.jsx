10136078b637c9fd0380b44476c14452bb82ae08
Isy <isy1997@freenet.de>
2024-03-17 12:46:12 +0100

    Set Ui for sign up

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,6 +1,10 @@
 // Import scss
 import "./login.scss"
 
+// MUI imports
+import { Box, Tab } from '@mui/material';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
+
 // Imports
 import { useContext, useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
@@ -13,9 +17,9 @@ const Login = () => {
     const [error, setError] = useState(false);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [value, setValue] = useState('1'); // for tabs
   
     const navigate = useNavigate();
-  
     const {dispatch} = useContext(AuthContext)
   
     const handleLogin = (e) => {
@@ -25,7 +29,6 @@ const Login = () => {
         .then((userCredential) => {
             // Signed in
             const user = userCredential.user;
-            const userId = user.uid;
 
             dispatch({type:"LOGIN", payload:user});
             navigate("/");
@@ -36,14 +39,36 @@ const Login = () => {
         });
     }
 
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
         <div className="login">
-            <form onSubmit={handleLogin}>
-                <input type="email" placeholder="Email" onChange={e=>setEmail(e.target.value)}/>
-                <input type="password" placeholder="Password" onChange={e=>setPassword(e.target.value)}/>
-                <button type="submit">Login</button>
-                {error && <span>Wrong email or password!</span>}
-            </form>
+            <TabContext value={value}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <TabList onChange={handleChange} aria-label="API tabs">
+                        <Tab label="Login" value="1" />
+                        <Tab label="Sign up" value="2" />
+                    </TabList>
+                </Box>
+                <TabPanel value="1">
+                    <form onSubmit={handleLogin}>
+                        <input type="email" placeholder="Email" onChange={e=>setEmail(e.target.value)}/>
+                        <input type="password" placeholder="Password" onChange={e=>setPassword(e.target.value)}/>
+                        <button type="submit">Login</button>
+                        {error && <span>Wrong email or password!</span>}
+                    </form>
+                </TabPanel>
+                <TabPanel value="2">
+                    <form onSubmit={handleLogin}>
+                        <input type="email" placeholder="Email" onChange={e=>setEmail(e.target.value)}/>
+                        <input type="password" placeholder="Password" onChange={e=>setPassword(e.target.value)}/>
+                        <button type="submit">Sign up</button>
+                        {error && <span>Something went wrong!</span>}
+                    </form>
+                </TabPanel>
+            </TabContext>
         </div>
     );
 };
