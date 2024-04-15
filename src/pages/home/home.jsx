52530ddcf449df6adae78cf14e7b1aad703086ec
Isy <isy1997@freenet.de>
2024-04-15 20:43:04 +0200

    Update layout

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,7 +5,6 @@ import "./home.scss"
 import Sidebar from "../../components/sidebar/sidebar";
 import Target from "../../components/target/target";
 import Chart from "../../components/chart/chart";
-import TriChart from "../../components/trichart/trichart";
 import HDLChart from "../../components/hdlchart/hdlchart";
 
 // Main
@@ -14,16 +13,12 @@ const Home = () => {
         <div className="home">
             <Sidebar className="sidebar"/>
             <div className="homeContainer">
-                <div className="homeTitle">Dashboard</div>
                 <div className="homeContent">
                     <Target />
                     <HDLChart />
                 </div>
                 <div className="homeContent">
                     <Chart />
-                </div>
-                <div className="homeContent">
-                    <TriChart />
                 </div>
             </div>
         </div>
