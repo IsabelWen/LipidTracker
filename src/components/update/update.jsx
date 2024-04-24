12cae9fe7c0419fc4e94dd5d0e6b72bbc77bc619
Isy <isy1997@freenet.de>
2024-04-24 19:34:50 +0200

    Create new update component

---
+++ src/components/update/update.jsx
@@ -0,0 +1,84 @@
+// Import scss
+import "./update.scss";
+
+// Import components
+import Sidebar from "../../components/sidebar/sidebar";
+
+// Imports
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { addDoc, collection } from "firebase/firestore";
+import { db, auth } from "../../firebase";
+
+// Main
+const Update = ({ inputs }) => {
+    const [data, setData] = useState("");
+    const navigate = useNavigate();
+    const user = auth.currentUser;
+    const userUID = user ? user.uid : null; 
+    
+    const handleInput = (e) => {
+        const id = e.target.id;
+        const value = e.target.value;
+
+        setData({ ...data, [id]:value})
+    }
+
+    const handleAdd = async (e) => {
+        e.preventDefault();
+        const formData = {};
+
+        for (const key in data) {
+            if (data[key]) {
+                formData[key] = data[key];
+            }
+        }
+
+        // Add userUID to formData
+        formData.userID = userUID;
+
+        try{
+            await addDoc(collection(db, "results"), formData);
+            navigate(-1);
+        }catch(err){
+            console.log(err);
+        }
+    };
+
+    return (
+        <div className="new">
+            <Sidebar className="sidebar"/>
+            <div className="newContainer">
+                <div className="top">
+                    <div className="title">Add New Test Result</div>
+                </div>
+                <div className="bottom">
+                    <div className="buttomContainer">
+                        <form onSubmit={handleAdd}>
+                            {inputs.map((input) => (
+                                <div className="formInput" key={input.id}>
+                                    <label>
+                                        {input.label}
+                                        <small>{input.unitLabel}</small>
+                                    </label>
+                                    <input id={input.id} 
+                                    type={input.type} 
+                                    placeholder={input.placeholder} 
+                                    min={input.min}
+                                    max={input.max}
+                                    maxLength={input.maxLength}
+                                    step={input.step}
+                                    required={input.required}
+                                    onChange={handleInput}/>
+                                </div>
+                            ))}
+                            <button type="submit">Add</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Update;
