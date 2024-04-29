17a39c4b702965daf2efd5e019614529c0df73e4
Isy <isy1997@freenet.de>
2024-04-29 21:20:17 +0200

    Add footer to faq page

--- src/pages/faq/faq.jsx
+++ src/pages/faq/faq.jsx
@@ -4,16 +4,20 @@ import "./faq.scss"
 // Import components
 import Sidebar from "../../components/sidebar/sidebar";
 import Questions from "../../components/questions/questions";
+import Footer from "../../components/footer/footer";
 
 // Main
 const FAQ = () => {
     return (
-        <div className="faq">
-            <Sidebar className="sidebar"/>
-            <div className="faqContainer">
-                <Questions />
+        <>
+            <div className="faq">
+                <Sidebar className="sidebar"/>
+                <div className="faqContainer">
+                    <Questions />
+                </div>
             </div>
-        </div>
+            <Footer></Footer>
+        </>
     );
 };
 
