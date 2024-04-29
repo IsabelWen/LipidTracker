5e3f12d0dcf6b198d47e8ca0994278c791613f40
Isy <isy1997@freenet.de>
2024-04-29 22:16:14 +0200

    Fix impressum pdf

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,11 +1,14 @@
 // Import scss
 import "./footer.scss";
 
+// Import PDF file
+import Impressum from "../../files/Impressum.pdf";
+
 // Main
 const Footer = () => {
     return (
         <div className="footer">
-            <a href="../../files/Impressum.pdf">Impressum</a>
+            <a href={Impressum} rel="noreferrer" target="_blank">Impressum</a>
         </div>
     );
 };
