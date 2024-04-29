d79ba75f53a5adbe2af55bdd1e7fc04af0158227
Isy <isy1997@freenet.de>
2024-04-29 21:19:06 +0200

    Add footer to results page

--- src/pages/results/results.jsx
+++ src/pages/results/results.jsx
@@ -4,16 +4,20 @@ import "./results.scss"
 // Import components
 import Sidebar from "../../components/sidebar/sidebar";
 import Resultstable from "../../components/resultstable/resultstable";
+import Footer from "../../components/footer/footer";
 
 // Main
 const Results = () => {
     return (
-        <div className="results">
-            <Sidebar className="sidebar"/>
-            <div className="resultsContainer">
-                <Resultstable />
+        <>
+            <div className="results">
+                <Sidebar className="sidebar"/>
+                <div className="resultsContainer">
+                    <Resultstable />
+                </div>
             </div>
-        </div>
+            <Footer></Footer>
+        </>
     );
 };
 
