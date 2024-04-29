78fa46e33d37d2abdab67efc250739a2596422e7
Isy <isy1997@freenet.de>
2024-04-29 21:17:02 +0200

    Add footer home page

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+// Import scss
+import "./footer.scss";
+
+// Main
+const Footer = () => {
+    return (
+        <div className="footer">
+            <a href="../../files/Impressum.pdf">Impressum</a>
+        </div>
+    );
+};
+
+export default Footer;
